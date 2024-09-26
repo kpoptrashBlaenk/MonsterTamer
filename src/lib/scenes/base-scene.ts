997eea77b3299621528eb25c46001a29a164b2c0
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-09-26 17:43:59 +0200

    Added: Color Scheme for Scene Logs

--- src/lib/scenes/base-scene.ts
+++ src/lib/scenes/base-scene.ts
@@ -2,14 +2,29 @@ import Phaser from "phaser";
 import {SceneKeys} from "./scene-keys";
 import {Controls} from "../../utils/controls";
 
+const SCENE_LOG_COLORS = Object.freeze({
+    TEST_SCENE: 'orange',
+    PRELOAD_SCENE: 'lightgray',
+    BATTLE_SCENE: 'red',
+    TITLE_SCENE: 'gold',
+    OPTIONS_SCENE: 'cyan',
+    MAIN_GAME_SCENE: 'deepskyblue',
+    MONSTER_PARTY_SCENE: 'mediumpurple',
+} as const)
+
+type SceneLogColors = typeof SCENE_LOG_COLORS[keyof typeof SCENE_LOG_COLORS]
+
 export class BaseScene extends Phaser.Scene {
+    private readonly color: SceneLogColors
     protected controls: Controls
 
-    constructor(config: { key: SceneKeys}) {
+    constructor(config: { key: SceneKeys }) {
         super(config)
-        if(this.constructor === BaseScene) {
+        if (this.constructor === BaseScene) {
             throw new Error('BaseScene is an abstract class and cannot be instantiated directly.')
         }
+
+        this.color = SCENE_LOG_COLORS[config.key]
     }
 
     init() {
@@ -32,6 +47,6 @@ export class BaseScene extends Phaser.Scene {
     }
 
     protected log(message: string) {
-        console.log(`%c${message}`, 'color: orange; background: black;')
+        console.log(`%c${message}`, `color: ${this.color}; background: black;`)
     }
 }
