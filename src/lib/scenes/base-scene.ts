3b35aa343351c95d2ed19e7706a66a63c5ebf52b
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-09-30 16:28:32 +0200

    Base Scene

--- src/lib/scenes/base-scene.ts
+++ src/lib/scenes/base-scene.ts
@@ -1,25 +1,53 @@
 import Phaser from "phaser";
 import {SceneKeys} from "./scene-keys";
+import {Controls} from "../../utils/controls";
 
 export class BaseScene extends Phaser.Scene {
+    protected controls: Controls
 
-    constructor(config: { key: SceneKeys}) {
+    constructor(config: { key: SceneKeys }) {
         super(config)
+        if (this.constructor === BaseScene) {
+            throw new Error('BaseScene is an abstract class and cannot be instantiated.');
+        }
     }
 
-    init() {
-        console.log(`[${this.constructor.name}:init] invoked`)
+    init(data: any | undefined) {
+        if (data) {
+            this.log(`[${this.constructor.name}:init] invoked, data provided: ${JSON.stringify(data)}`)
+            return
+        }
+        this.log(`[${this.constructor.name}:init] invoked`)
     }
 
     preload() {
-        console.log(`[${this.constructor.name}:preload] invoked`)
+        this.log(`[${this.constructor.name}:preload] invoked`)
     }
 
     create() {
-        console.log(`[${this.constructor.name}:create] invoked`)
+        this.log(`[${this.constructor.name}:create] invoked`)
+
+        this.controls = new Controls(this)
+        this.events.on(Phaser.Scenes.Events.RESUME, this.handleSceneResume, this)
+        this.events.once(Phaser.Scenes.Events.SHUTDOWN, this.handleSceneCleanup, this)
+
+        this.scene.bringToTop()
     }
 
-    update(time?: number) {
+    public handleSceneResume(data: any | undefined) {
+        if (data) {
+            this.log(`[${this.constructor.name}:handleSceneResume] invoked, data provided: ${JSON.stringify(data)}`)
+            return;
+        }
+        this.log(`[${this.constructor.name}:handleSceneResume] invoked`)
+    }
 
+    public handleSceneCleanup() {
+        this.log(`[${this.constructor.name}:handleSceneCleanup] invoked`);
+        this.events.off(Phaser.Scenes.Events.RESUME, this.handleSceneResume, this);
+    }
+
+    protected log(message: string) {
+        console.log(`%c${message}`, 'color: orange; background: black;');
     }
 }
