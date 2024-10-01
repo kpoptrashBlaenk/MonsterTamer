7b531ec70fb319e97389b7e4be4592551e5db603
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-01 15:23:10 +0200

    Update confirmation-menu.ts

--- src/common/confirmation-menu.ts
+++ src/common/confirmation-menu.ts
@@ -4,7 +4,6 @@ export const CONFIRMATION_MENU_OPTIONS = Object.freeze({
     YES: 'YES',
     NO: 'NO',
 })
-type ConfirmationMenuOptions = keyof typeof CONFIRMATION_MENU_OPTIONS
 
 export class ConfirmationMenu extends Menu {
     constructor(scene: Phaser.Scene) {
