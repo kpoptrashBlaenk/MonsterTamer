09ae44ff9add122ae946f9fc4777cd4fafcaedd4
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-21 12:00:47 +0200

    fuck this

--- src/battle/ui/battle-menu-config.ts
+++
@@ -1,8 +0,0 @@
-import Phaser from 'phaser'
-import {CUSTOM_FONTS} from "../../assets/font-keys"
-
-export const BATTLE_UI_TEXT_STYLE: Phaser.Types.GameObjects.Text.TextStyle = Object.freeze({
-    fontFamily: CUSTOM_FONTS.POKEROGUE,
-    color: 'black',
-    fontSize: '35px'
-})
